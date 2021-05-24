7a8332583cc55702b66a0ff9370a75eaf03c292f
Maverick Zhang <maverickzhang@berkeley.edu>
2021-05-24 14:02:06 -0400

    Sinusoidal brownian motion lol

--- brownian.py
+++ brownian.py
@@ -1,4 +1,4 @@
-from math import exp
+from math import exp, sin
 import random as r
 import matplotlib.pyplot as plt
 
@@ -14,8 +14,30 @@ class Brownian:
             delta_B = r.gauss(mu * delta_t, (sigma ** 2) * self.delta_t)
             self.range.append(self.range[i] + delta_B )
 
+class GeoBrownian:
+    def __init__(self, delta_t = 1, mu = 0, sigma = 1, n = 1000, S_0=1):
+        self.delta_t = delta_t
+        self.mu = mu
+        self.sigma = sigma
+        self.n = n
+        self.S_0 = S_0
+        B = Brownian(delta_t=self.delta_t, mu=self.mu - (self.sigma**2) / 2, sigma=self.sigma, n=self.n)
+        self.range = [self.S_0 * exp(b) for b in B.range]
+        # I don't know right now, it's weird
 
-B = Brownian(mu=0, sigma=1)
+class SineBrownian:
+    def __init__(self, delta_t = 1, mu = 0, sigma = 1, n = 1000, S_0 = 1):
+        self.delta_t = delta_t
+        self.mu = mu
+        self.sigma = sigma
+        self.n = n
+        self.S_0 = S_0
+        self.range = []
+        B = Brownian(delta_t=self.delta_t, mu=self.mu, sigma=self.sigma, n=self.n)
+        for i in range(len(B.range)):
+            self.range.append(self.S_0 * exp(sigma**2 / 2 * delta_t * i) * sin(B.range[i]))
+
+B = SineBrownian(delta_t=1, mu=1, sigma=1, n=1000)
 plt.plot(B.range)
 plt.show()
 
